5ef01cc1b6b9d7a956aaa1e09b749989a9ebc146
Andressa Raffler <andressa.raffler@gmail.com>
2022-11-13 15:43:06 -0300

    CRUD finished

--- src/main/java/dio/personapi/controller/PersonController.java
+++ src/main/java/dio/personapi/controller/PersonController.java
@@ -2,6 +2,7 @@ package dio.personapi.controller;
 
 import dio.personapi.dto.response.MessageResponseDTO;
 import dio.personapi.dto.request.PersonDTO;
+import dio.personapi.exception.PersonNotFoundException;
 import dio.personapi.service.PersonService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -21,6 +23,29 @@ public class PersonController {
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
         return personService.createPerson(personDTO);
     }
+    @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
+    }
+
+
+    @GetMapping("/{id}")
+    public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+        return personService.findById(id);
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
+        personService.deleteById(id);
+    }
+
+
 
 
 
--- src/main/java/dio/personapi/service/PersonService.java
+++ src/main/java/dio/personapi/service/PersonService.java
@@ -3,11 +3,14 @@ package dio.personapi.service;
 import dio.personapi.dto.response.MessageResponseDTO;
 import dio.personapi.dto.request.PersonDTO;
 import dio.personapi.entity.Person;
+import dio.personapi.exception.PersonNotFoundException;
 import dio.personapi.mapper.PersonMapper;
 import dio.personapi.repository.PersonRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -19,11 +22,41 @@ public class PersonService {
 
     public MessageResponseDTO createPerson(PersonDTO personDTO) {
         Person personToSave = personMapper.toModel(personDTO);
-
         Person savedPerson = personRepository.save(personToSave);
+        return createMessageRespose("Person with id" + savedPerson.getId() + " was created!");
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public PersonDTO findById(Long id) throws PersonNotFoundException {
+        Person personSaved = verifyIfPersonExists(id);
+        return personMapper.toDTO(personSaved);
+    }
+
+    public void deleteById(Long id) throws PersonNotFoundException {
+        Person personSaved = verifyIfPersonExists(id);
+        personRepository.delete(personSaved);
+    }
+    private Person verifyIfPersonExists(Long id) throws PersonNotFoundException {
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException(id));
+    }
+
+    private MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException {
+        verifyIfPersonExists(id);
+        Person personToUpdate = personMapper.toModel(personDTO);
+        Person updatedPerson = personRepository.save(personToUpdate);
+        return createMessageRespose("Person with id" + id + " was updated!");
+    }
+
+    private static MessageResponseDTO createMessageRespose(String text) {
         return MessageResponseDTO
                 .builder()
-                .message("Created person with ID " + savedPerson.getId())
+                .message(text)
                 .build();
     }
 }
